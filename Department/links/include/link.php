64aaf77f8509f9fde43b476d5698d30342ab6fc8
aj045045 <aj045045@gmail.com>
2023-03-24 20:01:04 +0530

    updaet

--- Department/links/include/link.php
+++ Department/links/include/link.php
@@ -11,6 +11,6 @@ echo '
 <script src="links/bs5/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="links/css/main.css">
 <link rel="stylesheet" href="links/css/global.css">
-<link rel="stylesheet" href="./links/css/tailwind/output.css">
+<link rel="stylesheet" href="./links/css/output.css">
 <title>Depart. of Computer science</title>
 <link rel="shortcut icon" href="image/logos/logo.webp" type="image/png">';
