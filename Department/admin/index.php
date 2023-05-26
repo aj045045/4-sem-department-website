bbdf9dd70c9a606a8396ea931f00a73d4e164b76
aj045045 <aj045045@gmail.com>
2023-05-26 19:01:22 +0530

    Cards

--- Department/admin/index.php
+++ Department/admin/index.php
@@ -3,24 +3,21 @@ $dns = "mysql:host=localhost;dbname=dcsdb";
 $dataBase = new PDO($dns, "root", "");
 try {
     //TODO - Cards of database
-    $countValue = array();
-    //SECTION - COURSES
-    // $dataBase->prepare("SELECT COUNT(course_id) FROM COURSE");
-    //SECTION -  Document : Result, old-papers, Circular
-    // SELECT COUNT(document-id) FROM document WHERE document is nothing;
-    $tmpValue = isset($tmp_data[3]) ? "WHERE $tmp_data[3]==$tmp_data[4]" : " ";
-    $dataBase->prepare("SELECT COUNT($tmp_data[0]) FROM $tmp_data[1] $tempValue");
-    //SECTION - Events
-
-    //SECTION - News
-
-    //SECTION - Feedback
-
-    //SECTION - Gallery Images
-
-    //SECTION - Temporary Question
-    //SECTION - Enquiry
-
+    //SECTION - COURSES, Document : Result, old-papers, Circular, Events, News, Feedback, Gallery Images, Temporary Question, Enquiry
+    $countValue = array(
+        "courses" => array("course_id", "course"), "results" => array("document_id", "documents", "document_category_id", 1), "papers" => array("document_id", "documents", "document_category_id", 2), "circular" => array("document_id", "documents", "document_category_id", 3),
+        "events" => array("event_id", "event", 1), "news" => array("news_id", "news"), "feedback" => array("feedback_id", "feedback"), "photos" => array("photo_id", "photos"), "questions" => array("tmp_question_id", "tmp_questions")
+    );
+    $resultValue = array();
+    foreach ($countValue as $key => $tmp_data) {
+        $queryResult = (isset($tmp_data[3]) && isset($tmp_data[4])) ? $dataBase->prepare("SELECT COUNT($tmp_data[0]) FROM $tmp_data[1] WHERE $tmp_data[3]==$tmp_data[4]") : $dataBase->prepare("SELECT COUNT($tmp_data[0]) FROM $tmp_data[1] ");
+        $queryResult->execute();
+        $val = $queryResult->fetch(PDO::FETCH_ASSOC);
+        foreach ($val as $resultKey => $value) {
+            $resultValue[$key] = $value;
+        }
+        $queryResult->closeCursor();
+    }
 } catch (Exception $exp) {
     echo "ERROR :" . $exp->getMessage() . " at line number " . $exp->getLine() . " in the file " . $exp->getFile();
 }
@@ -41,6 +38,8 @@ try {
 
 <body class=" tw-flex-col tw-flex tw-p-20 tw-gap-y-10 ">
     <!-- //NOTE - Div for Add / Update Data  -->
+    <div class=" tw-w-28 tw-uppercase tw-rounded-md tw-p-2 sm:tw-text-xl tw-text-md tw-bg-blue-700 tw-text-center tw-shadow-lg tw-shadow-blue-400 tw-text-white">LINKs</div>
+
     <div class="tw-flex-col tw-flex tw-gap-y-4">
         <div class="tw-flex-row tw-flex tw-gap-x-10">
             <a href="event.php" class="tw-bg-white tw-rounded-lg tw-cursor-pointer hover:tw-shadow-md  hover:tw-shadow-slate-400 tw-text-lg tw-pr-5 tw-py-2 tw-flex sm:tw-flex-row tw-flex-col tw-my-2">
@@ -71,24 +70,30 @@ try {
     </div>
 
     <!-- //NOTE - Div for Summary in cards  -->
-    <div class=" sm:tw-grid sm:tw-grid-rows-6 tw-flex tw-flex-col">
+    <!-- <div class=" sm:tw-grid sm:tw-grid-cols-6 "> -->
+    <div class=" tw-mt-40 tw-w-28 tw-uppercase tw-rounded-md tw-p-2 sm:tw-text-xl sm:tw-block tw-hidden tw-bg-blue-700 tw-shadow-lg tw-shadow-blue-400 tw-text-white">Summary</div>
+    <div class=" sm:tw-grid sm:tw-grid-cols-4 lg:tw-grid-cols-6 tw-gap-3 max-sm:tw-hidden">
         <?php
-        foreach ($countValue as $cv) { ?>
-            <div class="tw-flex tw-flex-col tw-border tw-border-slate-400 tw-rounded-md tw-w-32">
+        foreach ($resultValue as $key => $value) { ?>
+            <div class="tw-flex tw-flex-col tw-border tw-border-slate-500 tw-bg-slate-200 tw-rounded-md tw-w-32">
                 <div class="tw-text-center tw-mx-9 tw-my-6 tw-font-mono tw-text-3xl tw-flex tw-flex-row">
-                    <div class=" tw-border tw-p-3 tw-rounded-full tw-bg-slate-500 tw-shadow-xl tw-text-white">
-                        28
+                    <div class=" tw-border tw-p-3 tw-px-5 tw-rounded-full tw-bg-slate-500  tw-text-white">
+                        <?php
+                        echo $value;
+                        ?>
                     </div>
                 </div>
-                <div class=" tw-text-center tw-h-16 tw-font-sans tw-text-lg tw-mx-3">No. of programmers</div>
+                <div class=" tw-text-center tw-h-16 tw-font-sans tw-text-lg tw-mx-3">No. of <?php
+                                                                                            echo ucfirst($key);
+                                                                                            ?></div>
             </div>
         <?php
         }
         ?>
     </div>
+    <div class=" tw-mt-20 sm:tw-mt-40 tw-w-28 tw-uppercase tw-rounded-md tw-p-2 sm:tw-text-xl tw-text-lg tw-text-center tw-bg-blue-700 tw-shadow-lg tw-shadow-blue-400 tw-text-white">Details</div>
     <!--//NOTE -  Div for Details of Faculty, Awards and collaborators -->
-    <div class="tw-bg-slate-300 tw-text-center tw-rounded-lg tw-shadow-lg tw-shadow-slate-400">
-        <div class=" tw-capitalize tw-text-7xl tw-my-5 " style="font-family: Brush Script MT;">Details</div>
+    <div class="tw-bg-slate-300 tw-text-center tw-rounded-lg tw-shadow-lg tw-shadow-slate-500">
         <div class=" tw-flex-col tw-flex tw-h-26 tw-bg-white tw-rounded-lg tw-m-5 tw-shadow-lg tw-shadow-slate-400">
             <div class=" tw-capitalize tw-text-center tw-font-serif tw-text-4xl tw-my-5">Faculty</div>
         </div>
